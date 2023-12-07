765e63d554deeecac45b5133bee00a7b600d8969
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-07 20:21:10 +0300

    Added more code and variations of Hi Max and Hello others name

--- Example005_ConditionifElse/Program.cs
+++ Example005_ConditionifElse/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//////////
+Console.WriteLine("Please, enter your name and press ENTER ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "max")
+  {
+   Console.Write("OMG, This is Max, Hi!!!! ");
+  
+  }
+
+ else if (username.ToLower() == "макс")
+  {
+  Console.Write("OMG, This is Max, Hi!!!! ");
+  
+  }
+
+
+ else if (username.ToLower() == "максим")
+  {
+   Console.Write("OMG, This is Max, Hi!!!! ");
+  
+  }
+
+
+ else 
+  {
+   Console.Write("Hello ");
+   Console.Write(username); 
+   Console.Write("!");
+  }
+
+
+
+
