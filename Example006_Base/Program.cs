156fdfeecd6f445e28646d8f319e54571858702b
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-07 20:38:19 +0300

    Added searching of max number, v.01

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Программа для нахождения самой тяжелой гири при 
+//  помощи присваивания к переменной путями сравнения.
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Max number is ");
+Console.Write(max);
+Console.WriteLine(".");
