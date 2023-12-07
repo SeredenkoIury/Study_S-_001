92d9c8dbaa13ea59f26dbb0a711c171544dd3d57
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-07 19:32:43 +0300

    Added a random and better visual structures

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,14 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 100); // случайное число 1 2 3 ... 9
+Console.Write("First  number is "); // выводим в консоль число А
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100); // случайное число 1 2 3 ... 9
+Console.Write("Second number is "); // Выводим в консоль число B
+Console.WriteLine(numberB);
+
+// через доп переменную суммируем
 int result = numberA + numberB;
+
+// Выводим сумму
+Console.Write("The summ is ");
 Console.WriteLine(result);
+
