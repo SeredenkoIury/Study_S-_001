9a2536102d58a33bdf84cd6d878997e4bd8e50ef
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-08 17:05:01 +0300

    Example007_Magic added some corrections to the cartoon size

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,23 +1,23 @@
 ﻿
 Console.Clear();
 int xa = 1, ya = 1, 
-    xb = 1, yb = 15,
-    xc = 30, yc = 15;
+    xb = 1, yb = 10,
+    xc = 30, yc = 10;
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+Console.WriteLine(".");
 
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
+Console.WriteLine(".");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.WriteLine(".");
 
 int x = xa, y = xb;
 
 int count = 0; // Счетчик операций
 
-while(count < 1000000)
+while(count < 50000)
   {
     int what = new Random().Next(0, 3); //(0, 3) 0 1 2
     if(what == 0)
@@ -40,11 +40,10 @@ while(count < 1000000)
        
        
      Console.SetCursorPosition(x, y);
-     Console.WriteLine("+");
+     Console.WriteLine(".");
 
      count = count + 1;
   }
-
 
 
 
