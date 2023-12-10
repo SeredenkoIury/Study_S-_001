9f5f419e66314dbd31d9ad28bc3a4b14b0abe483
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-10 19:17:13 +0300

    Example011_ArrayLibrary program works with voids and arrays

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length; //adding and array
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); //adding a random number between 1 and 10
+        index++; //index = index + 1; 
+    }
+
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write("|");
+        position++;
+    }
+}
+
+int[] array = new int[10]; //Указание, что по умолчанию в массиве будет 10 элементов
+
+Console.Clear();
+Console.WriteLine();
+Console.WriteLine("----Random Array----");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("----Random Array----");
+Console.WriteLine();
