e50041e2ec7bbb21ecfa504dbda90d26ef32c77a
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-10 17:58:06 +0300

    Example009_IntroArrayPt2 progpram works and founds index of finding number

--- Example009_IntroArrayPt2/Program.cs
+++ Example009_IntroArrayPt2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 11, 52, 83, 49, 65, 69, 73, 4 };
+
+int n = array.Length; //info about how much elements
+int find = 73;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+        index++; //index = index + 1;
+}
