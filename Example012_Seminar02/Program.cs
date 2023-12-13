e66637d9c7c51cf899e11f01a255cb555bdcfcdc
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-13 20:09:42 +0300

    .\Example012_Seminar02\ completed task with 3 symb number and writing the 1st and the 2nd symb of the number

--- Example012_Seminar02/Program.cs
+++ Example012_Seminar02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 01.
+// Напишите задачу, которая принимает на вход
+// трехзначное число и удаляет вторую цифру этого числа.
+// примеры
+// a = 256 => 26
+// a = 891 => 81
+
+Console.Clear();
+int n = new Random ().Next(100, 1000); // генерация рандомного числа трехзначного
+                                       // функция next включает первое число но не 
+                                       включает последнее, поэтому 1000 а не 999
+
+
+int a = n/100;
+int b = n%10;
+
+Console.WriteLine("Randon Nimbwr is " + n);
+Console.Write("1st and 3rd symb together is " + a);
+Console.WriteLine(b);
