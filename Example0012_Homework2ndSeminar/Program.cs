cb869f4e6cac7b08909760f668f54fc0ad1e1713
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-17 20:04:48 +0300

    .\Example0012_Homework2ndSeminar\ project, task 3  - problems been fully fixed

--- Example0012_Homework2ndSeminar/Program.cs
+++ Example0012_Homework2ndSeminar/Program.cs
@@ -54,35 +54,37 @@ if (n > 9 && n < 100)
 {
 Console.Write("The highest symbol of the " + n + " is ");
 
-if (n1 < n2)
-    Console.WriteLine(n2);
-else
-    Console.WriteLine(n1);
-}
-else 
-{
-    Console.WriteLine("This is not a number between 10 and 99!");
-}
-// //Задача 4: Напишите программу, которая на вход принимает натуральное 
-// //число N, а на выходе показывает его цифры через запятую.
-// Console.Clear();
-// Console.WriteLine("Введите натуральное число и нажмите ENTER");
-// int N = Convert.ToInt32(Console.ReadLine());
-
-// if (N < 10)
-// {
-//     Console.WriteLine(N);
-// }
+// if (n1 < n2)
+//     Console.WriteLine(n2);
 // else
-// {
-//     while (N > 0)
-//     {
-//         int numbers = N % 10;
-//         N = N / 10;
-//         if (N > 0) Console.Write(numbers  + ", ");
-//         else  Console.Write(numbers);
-//     }
+//     Console.WriteLine(n1);
 // }
+// else 
+// {
+//     Console.WriteLine("This is not a number between 10 and 99!");
+// }
+
+
+//Задача 4: Напишите программу, которая на вход принимает натуральное 
+//число N, а на выходе показывает его цифры через запятую.
+Console.Clear();
+Console.WriteLine("Введите натуральное число и нажмите ENTER");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int numbers = N % 10;
+        N = N / 10;
+        if (N > 0) Console.Write(numbers  + ", ");
+        else  Console.Write(numbers);
+    }
+}
 
 
 
