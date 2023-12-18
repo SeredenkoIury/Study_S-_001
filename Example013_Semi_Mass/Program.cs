1c443748d613ecece34537de61929fb63a346ba6
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-18 15:06:05 +0300

    .\Example013_Semi_Mass - Added example of makeing a new random array with entering the size

--- Example013_Semi_Mass/Program.cs
+++ Example013_Semi_Mass/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///// Seminar 03, MASS
+
+Console.Clear();
+Console.WriteLine("Add the number of the ellements in array and press ENTER");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];// целые числа[массив] названиемассива = новый изцелыхчисел[колличество элементов];
+
+for (int i = 0; i < array.Length; i++) // array.Length = n-1;
+{
+    array[i] = new Random().Next(0,11); //[0,10]
+}
+
+Console.Write("|");
+
+for (int i = 0; i < array.Length; i++) 
+{
+    Console.Write(" " + array[i] + " | "); 
+}
+
