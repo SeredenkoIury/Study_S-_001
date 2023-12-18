9864d7fc0789d5f19227511c56a0f7baec7e4444
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-18 18:18:43 +0300

    .\Example013_Semi_Mass - Added a completed task 4.

--- Example013_Semi_Mass/Program.cs
+++ Example013_Semi_Mass/Program.cs
@@ -1,6 +1,6 @@
 ﻿///// Seminar 03, MASS
 
-//-----------------------------------------------------------------------------
+//==========================================================================================01
 // // makeing the size with console enter
 // Console.Clear();
 // Console.WriteLine("Add the number of the ellements in array and press ENTER");
@@ -23,16 +23,16 @@
 // //writing out the array ex2 
 // Console.WriteLine();
 // Console.WriteLine(string.Join(" | ", array)); //(строка.Соединение('разделитель между аргументами', массив))
-//---------------------------------------------------------------------------------------
 
 
 
+//==========================================================================================02
 // // Задайте массивю Найдите программу, которая определяет, 
 // // присутствует ли заданнное число в массиве. Программа должна выдать ответ да/нет.
 // // пример:
 // // [1 3 4 19 3], 8 => no 
 // // [-4 3 4 1], 3 => yes
-
+//---------------------------------
 // Console.Clear();
 // Console.WriteLine("Add the number of the ellements in the array and press ENTER");
 // int n = Convert.ToInt32(Console.ReadLine());
@@ -59,30 +59,51 @@
 //         break; // for() Full stop
 //     }
 
-
 // if (flag == false) 
 // Console.WriteLine("NO, There is no " + found + " in the array");
 
-// //--------------------------------------------
+
+// ==========================================================================================03
 // Задайте массив из 10 элементовб заполненный числами из промежутка
 // [-10, 10]. Замените отридцательные элементы положительными а положительные
 // отридцательными.
 // пример
 // [1,-5,6] => [-1;-5;6]
+//---------------------------------------------
+// Console.Clear();
+// int[] array = new int[10];
+// for(int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(-10,11);
 
-Console.Clear();
-int[] array = new int[10];
-for(int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(-10,11);
-    
-Console.WriteLine(string.Join(" | ", array));
+// Console.WriteLine(string.Join(" | ", array));
 
-for(int i = 0; i < array.Length; i++)
-    array[i] = array[i] * -1;
-    
-Console.WriteLine(string.Join(" | ", array));
+// for(int i = 0; i < array.Length; i++)
+//     array[i] = array[i] * -1;
+
+// Console.WriteLine(string.Join(" | ", array));
+
+//==========================================================================================04
+// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и 
+// последний элемент, второй и предпоследний и тд. Результат запишите в новый массив.
+// пример 
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5], (элемент 7 не имеет пары)
+//------------------------------------------------------
+// Console.Clear();
+// int n = new Random().Next(3, 11);
+// int[] array = new int[n];
+// for (int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(-10, 11);
+
+// Console.WriteLine(string.Join(" | ", array));
+
+// int[] array_new = new int[n / 2];
+
+// for (int i = 0; i < array_new.Length; i++)
+//     array_new[i] = array[i] * array[array.Length - 1 - i];
+
+// Console.WriteLine(string.Join(" | ", array_new));
 
 
-
-
+//==========================================================================================05
 
