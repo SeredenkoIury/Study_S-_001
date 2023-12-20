4b89a27136d677989725bc5c8366adfcd968a361
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-20 15:55:21 +0300

    .\Example015_Sem04_Methods_Homework\ finished tasn 1/3

--- Example015_Sem04_Methods_Homework/Program.cs
+++ Example015_Sem04_Methods_Homework/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// === SEREDENKO IURY DANILOVICH SEM04 FROM 11.12.2023 HOMEWORK ================
+
+// Задача 1 ========================================================================TASK 01
+// Напишите программу, которая бесконечно запрашивает целые числа 
+// с консоли. Программа завершается при вводе символа ‘q’ или 
+// при вводе числа, сумма цифр которого чётная.
+
+
+
+
+
+
+// Задача 2 ========================================================================TASK 02
+// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных
+// чисел в массиве.
+
+void InputArray(int[] array)// Makeing the random array with 3 symbls in 1 point
+
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100,999);
+}
+
+bool Check2Times(int x) //returning true if the number % 2 = 0
+{
+    
+         if (x % 2 == 0) return true;
+         return false;
+}
+
+void ReleaseArray(int [] array) // Haveing the number of points/2
+{
+    int count  = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if  (Check2Times(array [i]) == true)
+            count ++;
+    }
+    Console.WriteLine(count);
+}
+
+Console.Clear();
+Console.WriteLine("Add the number of elements in the Array and press ENTER");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine(string.Join(" | ", array));
+ReleaseArray(array);
+
+
+// Задача 3 ========================================================================TASK 03
+// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.).
