fa140b77d5702a19517aaddfc8d6ed645f6d1e8f
Iury Seredenko <iury.seredenko@gmail.com>
2023-12-21 03:01:34 +0300

    .\Example015_Sem04_Methods_Homework\ been finished and ready to send

--- Example015_Sem04_Methods_Homework/Program.cs
+++ Example015_Sem04_Methods_Homework/Program.cs
@@ -4,84 +4,52 @@
 // Напишите программу, которая бесконечно запрашивает целые числа 
 // с консоли. Программа завершается при вводе символа ‘q’ или 
 // при вводе числа, сумма цифр которого чётная.
+//-----------------------------------------------------------------
 
-// bool S1(int n) //returning true if should start full stop
+// Console.Clear();
+// Console.WriteLine("Add the number or 'q' to full stop and press ENTER");
+// while (true)
 // {
+//     string text = Console.ReadLine();
+//     Console.Clear();
 
-//          if (n == q) return true;
-//          return false;
+//     //Program full stop ---------------------------------------------\\
+//     if (text == "q") break;
+//     if (text == "Q") break;
+//     if (text == "й") break;
+//     if (text == "Й") break;
+//     //Program full stop ---------------------------------------------//
+
+
+//     int number;
+//     if (int.TryParse(text, out number)) // checkout if thats tne numbers
+//     {
+//         Console.WriteLine("Number is " + number);
+//         // calculateing the sum
+//         int sum = 0;
+//         while (number > 0)
+//         {
+//             sum = sum + number % 10;
+//             number = number / 10;
+//         }
+//         // sum/2 checkout----------------------------------
+//         if (sum % 2 == 0)
+//         {
+//             Console.WriteLine("the sum of parts of Number is even, sooo...");
+//             break;
+//         }
+
+//         else
+//         {
+//             Console.WriteLine("Number's ok");
+//             Console.WriteLine("Add one more number or q to stop and press ENTER");
+//         }
+//     }
+
+//     else // if there is no numbers or q
+//         Console.WriteLine("Incorrect enter. Please add the number or the Q and Press Enter");
 // }
-
-
-Console.Clear();
-Console.WriteLine("Add the number or q to stop the programm and press ENTER");
-while (true)
-{
-    
-    string text = Console.ReadLine();
-    Console.Clear();
-    //Program full stop ----------------------------------------\\
-    if (text == "q")
-    {
-    Console.WriteLine("  //  //  //  //  FULL STOP  // //  //  // ");
-    break;
-    }
-    if (text == "Q")
-    {
-    Console.WriteLine("  //  //  //  //  FULL STOP  // //  //  // ");
-    break;
-    }
-    if (text == "й")
-    {
-    Console.WriteLine(" //  //  //  //  FULL STOP  // //  //  // ");
-    break;
-    }
-    if (text == "Й")
-    {
-    Console.WriteLine("  //  //  //  //  FULL STOP  // //  //  // ");
-    break;
-    }
-    //Program full stop ----------------------------------------//
-
-
-    int number;
-    if (int.TryParse(text, out number)) // Проверка, является ли ввод числом{
-        {
-            Console.WriteLine("Number is " + number);
-            // calculateing the sum
-             int sum = 0;
-             while (number > 0)
-             {
-                sum = sum + number % 10;
-                number = number / 10;
-             }
-
-            // sum/2 checkout----------------------------------
-            if (sum % 2 == 0)
-             {
-             Console.WriteLine("the sum of parts of Number is even, sooo...");
-             Console.WriteLine("  //  //  //  //  FULL STOP  // //  //  // ");
-             break;
-             }
-
-
-            else  
-            {
-            
-            Console.WriteLine("Number's ok");
-            Console.WriteLine("Add one more number or q to stop and press ENTER");
-            }
-        }
-
-    else // Если ввод не является числом и не 'q', повторить запрос
-    Console.WriteLine("Incorrect enter. Please add the number or the Q and Press Enter");
-    
-}
-
-
-
-
-
+// Console.WriteLine("  //  //  //  //  FULL STOP  // //  //  // ");
 
 
 
@@ -99,7 +67,6 @@ while (true)
 
 // bool Check2Times(int x) //returning true if the number % 2 = 0
 // {
-
 //          if (x % 2 == 0) return true;
 //          return false;
 // }
@@ -125,6 +92,51 @@ while (true)
 // ReleaseArray(array);
 
 
+
+
 // Задача 3 ========================================================================TASK 03
 // Напишите программу, которая перевернёт одномерный массив 
 // (первый элемент станет последним, второй – предпоследним и т.д.).
+
+// Console.Clear();
+
+// void InputArray(int[] array)// Makeing the random array
+
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(1, 10);
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     int count = arr.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{arr[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SortingUpSideDown(int[] arr)
+// {
+
+//     for (int i = 0; i < arr.Length / 2; i++)
+//     {
+//         int memory;
+//         memory = arr[i];
+//         arr[i] = arr[arr.Length - 1 - i];
+//         arr[arr.Length - 1 - i] = memory;
+//     }
+
+// }
+
+// Console.Clear();
+// Console.WriteLine("Add the number of elements in the Array and press ENTER");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// int[] arr = new int[n];
+// InputArray(arr);
+// PrintArray(arr);
+// SortingUpSideDown(arr);
+// PrintArray(arr);
